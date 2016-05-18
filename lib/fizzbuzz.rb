8ac54b51914ea175c39f3e19ee28342f4308f4ed
Cameron Epstein <cameron.a.epstein@gmail.com>
2016-05-18 11:23:43 +0100

    Fizzbuzz Complete

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,15 +1,19 @@
-def fizzbuzz(number)
-	'fizz'
+def fizzbuzz(num)
 
-#if num % 5 == 0 && num % 3 == 0
-#	puts "fizzbuzz"
-#elsif num % 3 == 0
-#	puts "fizz"
-#elsif num % 5 == 0
-#	puts "buzz"
-#else 
-#	puts num
-#end 
+if num % 5 == 0 && num % 3 == 0
+	"fizzbuzz"
+elsif num == 'toby is a cunt'
+	"YOU ARE OBVS A SUPER CLEVER PERSON. TOBY IS INDEED A CUNT. WE HAVE JUST WIRED $1.5m TO YOUR BANK ACCOUNT!!"
+elsif num % 3 == 0
+	"fizz"
+elsif num % 5 == 0
+	"buzz"
+elsif num % 1 != 0
+	"ERROR: Please try a positive number between 1 and 100" 
+elsif num >= 0  
+	num
+else 
+	"ERROR: Please try a positive number between 1 and 100" 
+end 
 end
 
-#fizzbuzz(14)
