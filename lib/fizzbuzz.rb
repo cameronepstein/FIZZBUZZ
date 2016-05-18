113c103096b3b9f5d45c16c360bb4b2769610226
Cameron Epstein <cameron.a.epstein@gmail.com>
2016-05-18 10:56:13 +0100

    fizzbuzz test

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(number)
+	'fizz'
+
+#if num % 5 == 0 && num % 3 == 0
+#	puts "fizzbuzz"
+#elsif num % 3 == 0
+#	puts "fizz"
+#elsif num % 5 == 0
+#	puts "buzz"
+#else 
+#	puts num
+#end 
+end
+
+#fizzbuzz(14)
